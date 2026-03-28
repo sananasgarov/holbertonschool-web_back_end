6e8978c9e1f1e3800c99349d39aa2bbaf19f3d8f
sanan <prog58816@gmail.com>
2026-03-28 15:37:37 +0000

    4-mru_cache.py

---
+++ caching/4-mru_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+""" MRU Caching """
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """ MRU caching system """
+
+    def __init__(self):
+        """ Init """
+        super().__init__()
+        self.usage_order = []
+
+    def put(self, key, item):
+        """ Add item in cache """
+        if key is None or item is None:
+            return
+
+        # Əgər key artıq varsa → yenilə
+        if key in self.cache_data:
+            self.usage_order.remove(key)
+        else:
+            # yeni keydirsə və limit dolubsa
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                mru_key = self.usage_order.pop()
+                del self.cache_data[mru_key]
+                print("DISCARD:", mru_key)
+
+        self.cache_data[key] = item
+        self.usage_order.append(key)
+
+    def get(self, key):
+        """ Get item """
+        if key is None or key not in self.cache_data:
+            return None
+
+        # istifadə olundu → sona at
+        self.usage_order.remove(key)
+        self.usage_order.append(key)
+
+        return self.cache_data[key]

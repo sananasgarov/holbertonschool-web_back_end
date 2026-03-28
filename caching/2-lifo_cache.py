ac087f5d234c864f596687a65593a11b99b5e106
sanan <prog58816@gmail.com>
2026-03-28 17:25:24 +0000

    2-lifo_cache.py

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -1,36 +1,44 @@
-#!/usr/bin/env python3
-""" LIFO Caching """
-
+#!/usr/bin/python3
+""" LIFO Caching Module """
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """ LIFO caching system """
+    """
+    LIFOCache defines a LIFO caching system
+    """
 
     def __init__(self):
-        """ Init """
+        """ Initialize the cache """
         super().__init__()
-        self.stack = []  # son əlavə olunanları izləmək üçün
+        self.order = []
 
     def put(self, key, item):
-        """ Add item in cache """
+        """ 
+        Add an item in the cache using LIFO algorithm
+        """
         if key is None or item is None:
             return
 
-        # Əgər yeni keydirsə stack-ə əlavə et
-        if key not in self.cache_data:
-            self.stack.append(key)
-
+        # Check if key already exists to update order
+        if key in self.cache_data:
+            self.order.remove(key)
+        
+        # Add the item to cache and tracking list
         self.cache_data[key] = item
+        self.order.append(key)
 
-        # limit aşılırsa → sonuncunu sil
+        # Handle overflow
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            last_key = self.stack.pop()
+            # LIFO: Discard the second to last item added 
+            # (The last one added is the one we just put in, 
+            # so we discard the one that was previously the "newest")
+            last_key = self.order.pop(-2)
             del self.cache_data[last_key]
-            print("DISCARD:", last_key)
+            print(f"DISCARD: {last_key}")
 
     def get(self, key):
-        """ Get item """
-        if key is None:
-            return None
-        return self.cache_data.get(key)
+        """ 
+        Get an item by key
+        """
+        return self.cache_data.get(key, None)

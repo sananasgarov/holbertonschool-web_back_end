7957bbd1e1b2d31c441ba78dc487c23fe7d75006
root <sanan@gmail.com>
2026-04-02 13:35:09 +0000

    filtered_logger.py

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,18 +2,36 @@
 """
 filtered_logger.py
 """
+
 import re
-from typing import List
+import logging
+from typing import List, Tuple
+
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """Obfuscate specified fields in a log message."""
     pattern = r'(' + '|'.join(fields) + r')=([^' + re.escape(separator) + r']*)'
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: Tuple[str, ...]):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        original = super().format(record)
+        return filter_datum(list(self.fields), self.REDACTION, original, self.SEPARATOR)
+
+
 # Example usage when run directly
 if __name__ == "__main__":
-    fields = ["password", "date_of_birth"]
-    messages = [
-        "name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;",
-        "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"
-    ]
-    for message in messages:
-        print(filter_datum(fields, 'xxx', message, ';'))
+    message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
+    log_record = logging.LogRecord("my_logger", logging.INFO, None, None, message, None, None)
+    formatter = RedactingFormatter(fields=("email", "ssn", "password"))
+    print(formatter.format(log_record))

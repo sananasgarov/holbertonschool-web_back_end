496ce3ed946dfe9e5ff20f2bdf073d69967885d7
root <sanan@gmail.com>
2026-04-04 09:35:52 +0000

    filtered_logger.py

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,72 +1,23 @@
 #!/usr/bin/env python3
+"""
+Database connector
+"""
 
-import logging
-from typing import List
-import re
-"""Module for filtering and logging personal data."""
-
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
-
-
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    for field in fields:
-        pattern = f"{field}=.*?{separator}"
-        replace = f"{field}={redaction}{separator}"
-        message = re.sub(pattern, replace, message)
-    return message
-
-
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s " \
-             "%(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(
-            self.FORMAT
-        )
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        message = super().format(record)
-        return filter_datum(
-            self.fields,
-            self.REDACTION,
-            message,
-            self.SEPARATOR
-        )
-
-"""Module for filtering and logging personal data."""
-def get_logger() -> logging.Logger:
-    """Creates and returns a logger"""
-    logger = logging.getLogger("user_data")
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
-
-    handler = logging.StreamHandler()
-    handler.setFormatter(RedactingFormatter(PII_FIELDS))
-
-    logger.addHandler(handler)
-
-    return logger
 import os
 import mysql.connector
+from mysql.connector.connection_cext import CMySQLConnection
 
 
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """Returns a connection to the database."""
-    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+def get_db() -> CMySQLConnection:
+    """Return a MySQL database connection using environment variables."""
+    user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
     host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+    database = os.getenv("PERSONAL_DATA_DB_NAME")
 
     return mysql.connector.connect(
-        user=username,
+        user=user,
         password=password,
         host=host,
-        database=db_name
+        database=database,
     )

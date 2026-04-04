45a52847a30cbac1cec778e07a99b69c531dda4c
root <sanan@gmail.com>
2026-04-04 09:21:47 +0000

    filtered_logger.py

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
+
 import logging
 from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     for field in fields:
-        message = re.sub(f"{field}=.*?{separator}", f"{field}={redaction}{separator}", message)
+        pattern = f"{field}=.*?{separator}"
+        replace = f"{field}={redaction}{separator}"
+        message = re.sub(pattern, replace, message)
     return message
 
 
@@ -14,13 +18,21 @@ class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class """
 
     REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s " \
+             "%(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+        super(RedactingFormatter, self).__init__(
+            self.FORMAT
+        )
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         message = super().format(record)
-        return filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
+        return filter_datum(
+            self.fields,
+            self.REDACTION,
+            message,
+            self.SEPARATOR
+        )

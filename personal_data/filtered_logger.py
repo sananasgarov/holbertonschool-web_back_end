687fdf1d4560d1d0e6f2d2a75c9b933f78e5521c
root <sanan@gmail.com>
2026-04-04 09:33:48 +0000

    filtered_logger.py

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -53,3 +53,20 @@ def get_logger() -> logging.Logger:
     logger.addHandler(handler)
 
     return logger
+import os
+import mysql.connector
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Returns a connection to the database."""
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    return mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=db_name
+    )

f013903993cb3c4914e7ec1029e4af7fcf533d62
sananasgarov <prog58816@gmail.com>
2026-04-16 16:55:18 +0400

    Add logout functionality to session authentication

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -30,7 +30,8 @@ def before_request():
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
                       '/api/v1/forbidden/',
-                      '/api/v1/auth_session/login/']
+                      '/api/v1/auth_session/login/',
+                      '/api/v1/auth_session/logout/']
     if auth.require_auth(request.path, excluded_paths):
         if auth.authorization_header(request) is None and \
               auth.session_cookie(request) is None:
--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -47,3 +47,26 @@ def login():
     response.set_cookie(session_name, session_id)
 
     return response
+def destroy_session(self, request=None):
+    """Deletes the user session / logout"""
+
+    # request yoxdursa
+    if request is None:
+        return False
+
+    # cookie-dən session_id götür
+    session_id = self.session_cookie(request)
+    if session_id is None:
+        return False
+
+    # session_id user-a bağlıdır?
+    user_id = self.user_id_for_session_id(session_id)
+    if user_id is None:
+        return False
+
+    # session sil
+    if session_id in self.user_id_by_session_id:
+        del self.user_id_by_session_id[session_id]
+        return True
+
+    return False

2a7841f26ba6ead7c677a2012293549d32a6fd79
sananasgarov <prog58816@gmail.com>
2026-04-24 22:24:55 +0400

    Add method to retrieve user by session ID

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -51,3 +51,12 @@ class Auth:
         session_id = _generate_uuid()
         self._db.update_user(user.id, session_id=session_id)
         return session_id
+
+    def get_user_from_session_id(self, session_id: str = None) -> User:
+        """Returns the User based on session_id"""
+        if session_id is None:
+            return None
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None

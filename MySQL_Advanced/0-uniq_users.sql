28ff57f68dae3446087b55e460cee4da9bbfd3e7
root <senan@gmail.com>
2026-05-01 11:20:30 +0000

    0-uniq_users.sql

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,5 @@
+-- Creates a table 'users' with a unique email requirement.
+-- The script will not fail if the table already exists
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,

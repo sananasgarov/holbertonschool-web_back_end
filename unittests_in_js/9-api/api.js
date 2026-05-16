25ac8a52a242cfc12753a74b0ac22d8bdeebb8ef
sananasgarov <prog58816@gmail.com>
2026-05-16 12:26:04 +0400

    feat: implement payment system API with routes and tests

---
+++ unittests_in_js/9-api/api.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the payment system");
+});
+
+app.get(/^\/cart\/(\d+)$/, (req, res) => {
+  res.send(`Payment methods for cart ${req.params[0]}`);
+});
+
+app.listen(7865, () => {
+  console.log("API available on localhost port 7865");
+});
+
+module.exports = app;
---
+++ unittests_in_js/9-api/api.test.js
@@ -0,0 +1,48 @@
+const request = require("request");
+const { expect } = require("chai");
+
+describe("Index page", function () {
+  it("returns status code 200", function (done) {
+    request.get("http://localhost:7865", (err, res) => {
+      expect(res.statusCode).to.equal(200);
+      done(err);
+    });
+  });
+
+  it("returns the expected body", function (done) {
+    request.get("http://localhost:7865", (err, res, body) => {
+      expect(body).to.equal("Welcome to the payment system");
+      done(err);
+    });
+  });
+
+  it("returns the correct content type", function (done) {
+    request.get("http://localhost:7865", (err, res) => {
+      expect(res.headers["content-type"]).to.include("text/html");
+      done(err);
+    });
+  });
+});
+
+describe("Cart page", function () {
+  it("returns status code 200 when id is a number", function (done) {
+    request.get("http://localhost:7865/cart/12", (err, res) => {
+      expect(res.statusCode).to.equal(200);
+      done(err);
+    });
+  });
+
+  it("returns the expected body when id is a number", function (done) {
+    request.get("http://localhost:7865/cart/12", (err, res, body) => {
+      expect(body).to.equal("Payment methods for cart 12");
+      done(err);
+    });
+  });
+
+  it("returns 404 when id is not a number", function (done) {
+    request.get("http://localhost:7865/cart/hello", (err, res) => {
+      expect(res.statusCode).to.equal(404);
+      done(err);
+    });
+  });
+});
